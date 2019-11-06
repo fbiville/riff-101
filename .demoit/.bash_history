1d01bbe72f391f3788d1e6853f3744105b9f1c50
Florent Biville <fbiville@pivotal.io>
2019-11-06 17:06:19 +0100

    Add basic prez

---
+++ .demoit/.bash_history
@@ -0,0 +1,2 @@
+riff function create square --local-path ./sources/ --artifact square.js --image fbiville/square --tail
+watch riff function list
---
+++ sources/HundredDivider.java
@@ -0,0 +1,8 @@
+import java.util.function.Function;
+
+public class HundredDivider implements Function<Integer, Integer> {
+
+	public Integer apply(Integer input) {
+		return 100 / input;
+	}
+}
